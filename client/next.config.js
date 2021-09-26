fc854be637f0f753950f0a220e3a96e980a42b27
atultaz <atul.tiwari@me.com>
2021-09-26 18:35:03 +0530

    Environment Variable with next js

--- client/next.config.js
+++ client/next.config.js
@@ -1,2 +1,9 @@
-// const withCSS = require('@zeit/next-css');
-// module.exports = withCSS({});
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: 'NODE-REACT-AWS',
+    API: 'http://localhost:8000/api',
+    PRODUCTION: false,
+    DOMAIN: 'http://localhost:3000',
+    FB_APP_ID: 'JJSLKADFLKSAHFDSLKL',
+  },
+};
