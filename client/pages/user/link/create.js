d00519c30fd9a99e788b84e1d801ef1afcf6fb90
atultaz <atul.tiwari@me.com>
2021-10-05 13:07:52 +0530

    Submit link part 3

--- client/pages/user/link/create.js
+++ client/pages/user/link/create.js
@@ -10,7 +10,6 @@ handle change url
 handle change type
 handle change medium
 handle submit > post request to server
-show categories > checkbox
 show types > radio buttons
 show medium > radio buttons
 handle toggle > selecting categories
@@ -64,6 +63,37 @@ const Create = () => {
     console.log('POST to server');
   };
 
+  const handleToggle = (c) => () => {
+    // return the first index or -1
+    const clickedCategory = categories.indexOf(c);
+    const all = [...categories];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    console.log('all >> categories', all);
+    setState({ ...state, categories: all, success: '', error: '' });
+  };
+
+  // show categories > checkbox
+  const showCategories = () => {
+    return (
+      loadedCategories &&
+      loadedCategories.map((c, i) => (
+        <li className='list-unstyled' key={c._id}>
+          <input
+            type='checkbox'
+            onChange={handleToggle(c._id)}
+            className='mr-2'
+          />
+          <label className='form-check-label'>{c.name}</label>
+        </li>
+      ))
+    );
+  };
+
   // link create form
   const submitLinkForm = () => (
     <form onSubmit={handleSubmit}>
@@ -102,11 +132,17 @@ const Create = () => {
         </div>
       </div>
       <div className='row'>
-        <div className='col-md-4'>xxx</div>
+        <div className='col-md-4'>
+          <div className='form-group'>
+            <label className='text-muted ml-4'>Category</label>
+            <ul style={{ maxHeight: '100px', overflowY: 'scroll' }}>
+              {showCategories()}
+            </ul>
+          </div>
+        </div>
         <div className='col-md-8'>{submitLinkForm()}</div>
       </div>
-      {JSON.stringify(title)}
-      {JSON.stringify(url)}
+      {JSON.stringify(categories)}
     </Layout>
   );
 };
