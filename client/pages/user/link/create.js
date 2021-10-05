e2a775b871b4baffb94615bbcd36d5ea98d03519
atultaz <atul.tiwari@me.com>
2021-10-05 12:34:10 +0530

    Submit link page 2

--- client/pages/user/link/create.js
+++ client/pages/user/link/create.js
@@ -14,7 +14,6 @@ show categories > checkbox
 show types > radio buttons
 show medium > radio buttons
 handle toggle > selecting categories
-link create form
 return > show create forms, categories checkbox, radio buttons, success/error messages etc
 get token of the logged in user - required to create link
  */
@@ -53,15 +52,61 @@ const Create = () => {
     setState({ ...state, loadedCategories: response.data });
   };
 
+  const handleTitleChange = (e) => {
+    setState({ ...state, title: e.target.value, error: '', success: '' });
+  };
+
+  const handleURLChange = (e) => {
+    setState({ ...state, url: e.target.value, error: '', success: '' });
+  };
+
+  const handleSubmit = async (e) => {
+    console.log('POST to server');
+  };
+
+  // link create form
+  const submitLinkForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label className='text-muted'>Title</label>
+        <input
+          type='text'
+          className='form-control'
+          onChange={handleTitleChange}
+          value={title}
+        />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>URL</label>
+        <input
+          type='url'
+          className='form-control'
+          onChange={handleURLChange}
+          value={url}
+        />
+      </div>
+      <div>
+        <button className='btn btn-outline-warning' type='submit'>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <Layout>
       <div className='row'>
         <div className='col-md-12'>
           <h1>Submit Link/URL</h1>
           <br />
-          {JSON.stringify(loadedCategories)}
         </div>
       </div>
+      <div className='row'>
+        <div className='col-md-4'>xxx</div>
+        <div className='col-md-8'>{submitLinkForm()}</div>
+      </div>
+      {JSON.stringify(title)}
+      {JSON.stringify(url)}
     </Layout>
   );
 };
