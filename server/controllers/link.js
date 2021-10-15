d7fc0ae7953c6ea6fec1e7d6eef5a2d6da85b38e
atultaz <atul.tiwari@me.com>
2021-10-15 17:33:19 +0530

    Mass email to users on new link publish

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -1,15 +1,25 @@
 const Link = require('../models/link');
+const User = require('../models/user');
+const Category = require('../models/category');
 const slugify = require('slugify');
+const { linkPublishedParams } = require('../helpers/email');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 
 exports.create = (req, res) => {
   const { title, url, categories, type, medium } = req.body;
-  // console.table({ title, url, categories, type, medium });
+  console.table({ title, url, categories, type, medium });
   const slug = url;
   let link = new Link({ title, url, categories, type, medium, slug });
   // posted by user
   link.postedBy = req.user._id;
-  // categories
-
   // save link
   link.save((err, data) => {
     if (err) {
@@ -18,6 +28,31 @@ exports.create = (req, res) => {
       });
     }
     res.json(data);
+    // find all users in the category
+    User.find({ categories: { $in: categories } }).exec((err, users) => {
+      if (err) {
+        throw new Error(err);
+        console.log('Error finding users to send email on link publish');
+      }
+      Category.find({ _id: { $in: categories } }).exec((err, result) => {
+        data.categories = result;
+
+        for (let i = 0; i < users.length; i++) {
+          const params = linkPublishedParams(users[i].email, data);
+          const sendEmail = ses.sendEmail(params).promise();
+
+          sendEmail
+            .then((success) => {
+              console.log('email submitted to SES ', success);
+              return;
+            })
+            .catch((failure) => {
+              console.log('error on email submitted to SES  ', failure);
+              return;
+            });
+        }
+      });
+    });
   });
 };
 
