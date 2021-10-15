514622051944c57128b89d4aa33b7a71a4bf46a9
atultaz <atul.tiwari@me.com>
2021-10-15 21:37:01 +0530

    most popular and trending

--- server/routes/link.js
+++ server/routes/link.js
@@ -22,6 +22,8 @@ const {
   update,
   remove,
   clickCount,
+  popular,
+  popularInCategory,
 } = require('../controllers/link');
 
 // routes
@@ -35,7 +37,8 @@ router.post(
 );
 router.post('/links', requireSignin, adminMiddleware, list);
 router.put('/click-count', clickCount);
-
+router.get('/link/popular', popular);
+router.get('/link/popular/:slug', popularInCategory);
 router.get('/link/:id', read);
 router.put(
   '/link/:id',
